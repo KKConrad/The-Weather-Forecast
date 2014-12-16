0fcab9e8fdf32363e1aacf996d77662fc109b7e3
KC <chocokrizx@gmail.com>
2014-12-15 17:11:15 -0700

    WIP: will be adding more code after html/css layout is more refined

--- forecast.js
+++ forecast.js
@@ -13,3 +13,15 @@ jQuery(document).ready(function($) {
     });
 });
 
+//to-do list:
+
+function setWeather(weather) {
+    //be able to set weather classes manually
+}
+
+function setTemp(temperature) {
+    //also be able to set the temperature.
+}
+
+//get userWeather when they enter in a location using zipcode, or city & state,
+//and then be able to have the API read the string and return information
