455505b7b80ef3f992f4e5db2b7c430b8a6a1721
KC <chocokrizx@gmail.com>
2015-01-01 18:26:58 -0700

    Organize dom ready, WU updates with zip code data, parse weather function

--- forecast.js
+++ forecast.js
@@ -1,21 +1,14 @@
 jQuery(document).ready(function($) {
-    $.ajax({
-        url : "http://api.wunderground.com/api/09d47c2436583517/geolookup/conditions/q/IA/Cedar_Rapids.json",
-        dataType : "jsonp",
-        success : function(parsed_json) {
-            var location = parsed_json['location']['city'];
-            temp_f = parsed_json['current_observation']['temp_f'];
-            setTemp(temp_f);
-            console.log(parsed_json);
-
-           /* alert("Current temperature in " + location + " is: " + temp_f);*/
-        }
-
-
+    $("#locationsubmit").click(function() {
+        console.log('clicked');
+        updateWeather();
+        return false;
     });
 });
 
+/*
 var temp_f;
+*/
 
 function setWeather(weather) {
     var weatherSelector = "#forecast";
@@ -30,6 +23,34 @@ function setTemp(temperature) {
     var tempSelector = "#todaystemp";
     var tempset = $(tempSelector);
     tempset.html(temperature)
+}
+
+function parseWeather(wuWeather) {
+    if (wuWeather === "Overcast" || wuWeather === "Mostly Cloudy" || wuWeather === "Heavy Fog") {
+        return "overcastweather";
+    } else if (wuWeather === "Clear") {
+        return "sunnyweather";
+}
+
+function updateWeather() {
+    $.ajax({
+        url : "http://api.wunderground.com/api/09d47c2436583517/geolookup/conditions/q/" + getZipcode() + ".json",
+        dataType : "jsonp",
+        success : function(parsed_json) {
+            var location = parsed_json['location']['city'];
+            var temp_f = parsed_json['current_observation']['temp_f'];
+            var weather = parsed_json['current_observation']['weather'];
+            setTemp(temp_f);
+            setWeather(parseWeather(weather));
+            console.log(parsed_json);
+
+            /* alert("Current temperature in " + location + " is: " + temp_f);*/
+        }
+    });
+}
+
+function getZipcode() {
+    return $("#zipcode").val();
 }
 
 
